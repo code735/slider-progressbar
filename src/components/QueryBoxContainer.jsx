d51fb8ac17941ed4598dd2aa99dd2ebc42f62e5d
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-05-16 13:29:00 +0530

    Initial commit

---
+++ src/components/QueryBoxContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+
+export default function QueryBoxContainer() {
+    return (
+        <Flex
+            height={'100vh'}
+            justifyContent={'center'}
+            alignItems={'center'}
+        >
+            <Box
+                height={'90%'}
+                w={'80%'}
+                bg={'white'}
+                borderRadius={'15'}
+            >
+
+            </Box>
+        </Flex>
+    )
+}
